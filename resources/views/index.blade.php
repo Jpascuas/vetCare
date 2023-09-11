4584b3fd02124fde64f37b75cb914aafe11dbcd0
JPascuas <juandapascuas16@gmail.com>
2023-09-11 10:55:19 -0500

    Se agrega plantilla adminLTE para la vista

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,12 @@
+@extends('layout.plantilla')
 <!DOCTYPE html>
 <html lang="en">
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>App Personas</title>
+ <title>App Mascotas</title>
 </head>
 <body>
- <h1>Bienvenidos a mi proyecto Laravel</h1>
+ <h1></h1>
 </body>
 </html>
